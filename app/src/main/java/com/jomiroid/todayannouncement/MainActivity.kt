96699244afe4c25275e298c222c4ae42f4cb30b2
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2024-08-22 09:27:00 +0900

    TodayAnnouncement : 03. SocketServer를 이용하여 간단한 소켓 서버 구현해보기(2)

--- app/src/main/java/com/jomiroid/todayannouncement/MainActivity.kt
+++ app/src/main/java/com/jomiroid/todayannouncement/MainActivity.kt
@@ -13,38 +13,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Thread {
-            val port = 8080
-            val server = ServerSocket(port)
-
-            val socket = server.accept()
-
-            socket.getInputStream()     // 클라이언트로 부터 들어오는 스트림 == 클라이언트의 socket.outputStream
-            socket.getOutputStream()    // 클라이언트에게 데이터를 주는 스트림 == socket.inputStream
-
-
-            val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
-            val printer = PrintWriter(socket.getOutputStream())
-
-            var input: String = "-1"
-
-            while (input != null && input !="") {
-                input = reader.readLine()
-            }
-
-            Log.e("SERVER", "READ DATA $input")
-
-            printer.println("HTTP/1.1 200 OK")
-            printer.println("Contetnt-Type: text/html\r\n")
-
-            printer.println("<h1>Hello World</h1>")
-            printer.println("\r\n")
-            printer.flush()
-            printer.close()
-
-            reader.close()
-
-            socket.close()
-        }
     }
 }

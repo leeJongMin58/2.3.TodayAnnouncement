10a618f544591d95e1f5568a441c0ddc7d605a73
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2024-08-22 10:29:37 +0900

    TodayAnnouncement : 04. Socket을 활용하여 Client 앱 만들기

--- app/src/main/java/com/jomiroid/todayannouncement/MainActivity.kt
+++ app/src/main/java/com/jomiroid/todayannouncement/MainActivity.kt
@@ -7,11 +7,39 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.ServerSocket
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread {
+            try {
+                // 에뮬레이터에서 테스트 할떄
+                val socket = Socket("10.0.2.2", 8080)
+                val printer = PrintWriter(socket.getOutputStream())
+                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+
+                printer.println("GET / HTTP/1.1")
+                printer.println("Host: 127.0.0.1:8080")
+                printer.println("User-Agent: android")
+                printer.println("\r\n")
+                printer.flush()
+
+                var input: String? = "-1"
+                while (input != null) {
+                    input = reader.readLine()
+                    Log.e("Client", input)
+                }
+
+                reader.close()
+                printer.close()
+                socket.close()
+            } catch (e: Exception) {
+                Log.e("Client", e.toString())
+            }
+        }.start()
 
     }
 }

d100c68248492650c70b5214aa696c9dded969f0
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2024-08-22 10:44:30 +0900

    TodayAnnouncement : 05. Okhttp 를 이용하여 Client 앱 만들기

--- app/src/main/java/com/jomiroid/todayannouncement/MainActivity.kt
+++ app/src/main/java/com/jomiroid/todayannouncement/MainActivity.kt
@@ -3,7 +3,13 @@ package com.jomiroid.todayannouncement
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.ServerSocket
@@ -14,32 +20,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Thread {
-            try {
-                // 에뮬레이터에서 테스트 할떄
-                val socket = Socket("10.0.2.2", 8080)
-                val printer = PrintWriter(socket.getOutputStream())
-                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+        val client = OkHttpClient()
 
-                printer.println("GET / HTTP/1.1")
-                printer.println("Host: 127.0.0.1:8080")
-                printer.println("User-Agent: android")
-                printer.println("\r\n")
-                printer.flush()
+        val request: Request = Request.Builder()
+            .url("http://10.0.2.2:8080")
+            .build()
 
-                var input: String? = "-1"
-                while (input != null) {
-                    input = reader.readLine()
-                    Log.e("Client", input)
-                }
-
-                reader.close()
-                printer.close()
-                socket.close()
-            } catch (e: Exception) {
+        val callback = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
                 Log.e("Client", e.toString())
             }
-        }.start()
 
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    Log.e("Client", "${response.body?.string()}")
+                }
+            }
+        }
+
+        client.newCall(request).enqueue(callback)
+
+//        Thread {
+//            try {
+//                // 에뮬레이터에서 테스트 할떄
+//                val socket = Socket("10.0.2.2", 8080)
+//                val printer = PrintWriter(socket.getOutputStream())
+//                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+//
+//                printer.println("GET / HTTP/1.1")
+//                printer.println("Host: 127.0.0.1:8080")
+//                printer.println("User-Agent: android")
+//                printer.println("\r\n")
+//                printer.flush()
+//
+//                var input: String? = "-1"
+//                while (input != null) {
+//                    input = reader.readLine()
+//                    Log.e("Client", input)
+//                }
+//
+//                reader.close()
+//                printer.close()
+//                socket.close()
+//            } catch (e: Exception) {
+//                Log.e("Client", e.toString())
+//            }
+//        }.start()
     }
 }
